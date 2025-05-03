516d83243eba4a7f22bea5b8cd11e0b3c771237f
Shubham Trigunait <shubhtrig@gmail.com>
2025-05-03 14:09:27 +0200

    03-05 this weeks

---
+++ grind75/containsDuplicate.java
@@ -0,0 +1,14 @@
+/**
+ * https://leetcode.com/problems/contains-duplicate/
+ */
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num))
+                return true;
+        }
+
+        return false;
+    }
+}
